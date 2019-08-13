f84a1a0c64066ffca8b54eba0747a18fc58206d1
reyphilipp <reyphilipp@freenet.de>
2019-08-13 20:47:43 +0200

    Add components addressList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddressListComponent } from './components/address-list/address-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressListComponent
   ],
   imports: [
     BrowserModule,
