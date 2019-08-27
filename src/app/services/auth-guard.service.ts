3d9c642e355d17ae64b4ce20a785adc7de572ecf
reyphilipp <reyphilipp@freenet.de>
2019-08-27 20:40:51 +0200

    Add Token AuthGuard

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,34 +1,18 @@
-import { UserResponse } from './../models/user';
-import { HttpClient } from '@angular/common/http';
+import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
-import { User } from '../models/user';
-import { environment } from 'src/environments/environment';
-import { longStackSupport } from 'q';
+import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
-  user: User;
-  authUrl = environment.authUrl;
+export class AuthGuardService implements CanActivate {
+  constructor(public auth: AuthService, public router: Router) {}
 
-  constructor(private http: HttpClient) {}
-
-  getToken(): string {
-    return localStorage.getItem('token');
-  }
-
-  login(username: string, password: string) {
-    this.http.post<UserResponse>(`${this.authUrl}login/`, {username, password}).subscribe(data => {
-      this.user = data.user;
-      localStorage.setItem('token', data.token);
-    });
-  }
-
-  logout() {
-    this.http.post(`${this.authUrl}logout/`, {}).subscribe(data => {
-      console.log(data);
-      localStorage.removeItem('token');
-    });
+  canActivate(): boolean {
+    if (!this.auth.getToken()) {
+      this.router.navigate(['login']);
+      return false;
+    }
+    return true;
   }
 }
